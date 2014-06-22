1c35041f9d16ddd1c052773d7544f251dc62c33e
wx <wxatnju@gmail.com>
2014-06-22 19:48:07 +0800

    新建项目BookLendSystem

---
+++ src/edu/nju/booklend/data/po/UserPO.java
@@ -0,0 +1,24 @@
+package edu.nju.booklend.data.po;
+
+public abstract class UserPO {
+
+	private String userName;
+	private String password;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
